6b5474b1a627a0d5d95e70e12cc07d34f802f628
Nithish0003 <nits000333@gmail.com>
2025-01-28 17:38:15 +0530

    path

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,6 +1,6 @@
 const express = require(`express`);
 const { dashboard } = require("../controllers/dashboard.controller");
-const authToken = require("./middleware/authToken");
+const authToken = require("../middleware/authToken");
 const router = express.Router();
 
 router.get("/:userId", authToken, dashboard);
